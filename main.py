a91538ddd76717ca3636f9a68c63ac9c540f6f42
Cansel İslamoğlu <bicanis53@gmail.com>
2023-08-22 18:06:14 +0300

    Second commit added

--- main.py
+++ main.py
@@ -16,3 +16,21 @@ def body_mass_index():
        sonuç_label.config(text=sonuç_string)
       except:
        sonuç_label.config(text="değer girin")
+
+
+
+kilo_label =tkinter.Label(text="kilonuzu girin:")
+kilo_label.pack()
+kilo_giris=tkinter.Entry(width=15)
+kilo_giris.pack()
+
+boy_label=tkinter.Label(text="boyunuzu girin:")
+boy_label.pack()
+boy_giris=tkinter.Entry(width=15)
+boy_giris.pack()
+
+hesaplama_button=tkinter.Button(text="Hesaplama",command=body_mass_index)
+hesaplama_button.pack()
+
+sonuç_label=tkinter.Label()
+sonuç_label.pack()
